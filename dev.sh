f7e2ce8f372dabcfc03c555221d5561237c9b6cf
0scarB <oscarb@protonmail.com>
2024-07-28 01:04:49 +0200

    Add browser implementation

--- dev.sh
+++ dev.sh
@@ -7,9 +7,12 @@ EXEC=2
 DEV=$(( COMPILE | EXEC ))
 
 compile() {
+    #
+    # x86-64 Linux
+    #
+    out_file='gol-x86-64-bit-linux'
     gcc -march=x86-64 -DLINUX_64_BIT \
         -Werror \
-        -Os \
         -static \
         -s \
         -nostartfiles -nostdlib \
@@ -19,10 +22,15 @@ compile() {
         -Wl,--hash-style=sysv \
         -Wl,--build-id=none \
         gol.c \
-        -o gol-x86-64-linux
-    strip --remove-section=.note.gnu.property gol-x86-64-linux
-    echo "Compiled x86-64 linux. Size in bytes: $( wc -c gol-x86-64-linux )"
+        -o "$out_file"
+    strip --remove-section=.note.gnu.property "$out_file"
+    echo "Compiled ${out_file} for x86, 64-bit, Linux. Size in bytes: $( \
+        wc -c "$out_file" | cut -d ' ' -f1 )"
 
+    #
+    # x86 32-bit Linux
+    #
+    out_file='gol-x86-32-bit-linux'
     gcc -m32 -march=i686 -DLINUX_32_BIT \
         -Werror \
         -Os \
@@ -35,13 +43,45 @@ compile() {
         -Wl,--hash-style=sysv \
         -Wl,--build-id=none \
         gol.c \
-        -o gol-i686-linux
-    strip --remove-section=.note.gnu.property --remove-section=.got.plt gol-i686-linux
-    echo "Compiled x86 32-bit linux. Size in bytes: $( wc -c gol-i686-linux )"
+        -o "$out_file"
+    strip --remove-section=.note.gnu.property --remove-section=.got.plt "$out_file"
+    echo "Compiled ${out_file} for x86, 32-bit, Linux. Size in bytes: $( \
+        wc -c "$out_file" | cut -d ' ' -f1 )"
+
+    #
+    # Web = WebAssembly + HTML
+    #
+    out_file='gol-browser.html'
+
+    # Compile C to WebAssembly binary
+    clang --target=wasm32 -DWEB \
+        -Werror \
+        -Os \
+        -static \
+        -s \
+        -nostartfiles -nostdlib \
+        gol.c \
+        -o gol-browser.wasm
+
+    # Embed WebAssembly as base64-encoded string in the HTML file
+    embed_line_no="$(                               \
+        grep --line-number WASM_BASE64 gol-browser.html \
+        | cut -d ':' -f1                            \
+        | tail --lines 1                            )"
+    head -n "$(( $embed_line_no - 1 ))" gol-browser.html              > gol-browser-new.html
+    echo "var WASM_BASE64=\"$( base64 --wrap 0 gol-browser.wasm )\"" >> gol-browser-new.html
+    tail   +"$(( $embed_line_no + 1 ))" gol-browser.html             >> gol-browser-new.html
+    mv gol-browser-new.html "$out_file"
+
+    # Remove WebAssembly binary
+    rm gol-browser.wasm
+
+    echo "Compiled ${out_file} for the Browser. Size in bytes: $( \
+        wc -c "$out_file" | cut -d ' ' -f1 )"
 }
 
 exec_() {
-    ./gol-x86-64-linux
+    ./gol-x86-64-bit-linux
 }
 
 main() {
--- gol.c
+++ gol.c
@@ -2,108 +2,131 @@
 #define U16 unsigned short
 #define I16   signed short
 
+static void setup(void);
+#ifdef WEB
+    __attribute__((export_name("update")))
+    void update(void);
+
+    __attribute__((import_name("output")))
+    void write_stdout(char* buf, U16 buf_size);
+
+    __attribute__((import_name("clearTerm")))
+    void clear_terminal(void);
+
+    __attribute__((import_name("updateEveryNSecs")))
+    void update_every_n_secs(float secs);
+#else
+    static void update(void);
+    static void write_stdout(char* buf, U16 buf_size);
+    static void clear_terminal(void);
+    static void update_every_n_secs(float secs);
+#endif
+
 #define ALIVE_TEXT "##"
 #define  DEAD_TEXT ". "
+#define WIDTH  20
+#define HEIGHT 30
 
-static U8 mem_region[16*4096] = {0};
+static const U8 GLIDER[3][3] = {
+    {0, 1, 0},
+    {0, 0, 1},
+    {1, 1, 1},
+};
 
-static void write_stdout(char* buf, U16 buf_size);
-static void sleep_fractional(float secs);
-static void clear_terminal(void);
+static U8 bufs[2][HEIGHT][WIDTH];
+static U8 buf_idx_toggle = 0;
+#define OLD_BUF bufs[buf_idx_toggle^1]
+#define NEW_BUF bufs[buf_idx_toggle  ]
+static char text_buf[16*WIDTH*HEIGHT];
 
 void _start(void) {
-    U16 w = 20;
-    U16 h = 30;
+    setup();
+    update_every_n_secs(0.1);
+}
 
-    static const U8 GLIDER[3][3] = {
-        {0, 1, 0},
-        {0, 0, 1},
-        {1, 1, 1},
-    };
-
-    U8 (*bufs)[h][w] = (U8 (*)[h][w]) mem_region;
-    U8 buf_idx_toggle = 0;
-    #define OLD_BUF bufs[buf_idx_toggle^1]
-    #define NEW_BUF bufs[buf_idx_toggle  ]
+void setup(void) {
     for (U16 y = 0; y < 3; ++y)
         for (U16 x = 0; x < 3; ++x)
             OLD_BUF[y][x] = GLIDER[y][x];
+}
 
-    char* text_buf = (char*) mem_region + 2*w*h;
+void update(void) {
+    char* text_ptr = text_buf;
 
-    while (1) {
-        char* text_ptr = text_buf;
+    for (I16 y = 0; y < HEIGHT; ++y) {
+        for (I16 x = 0; x < WIDTH; ++x) {
+            U8 live_neighbors = 0;
+            for (I16 ny = y-1; ny <= y+1; ++ny)
+                for (I16 nx = x-1; nx <= x+1; ++nx)
+                    live_neighbors +=
+                        OLD_BUF[(ny+HEIGHT) % HEIGHT]
+                               [(nx+WIDTH ) % WIDTH ];
+            live_neighbors -= OLD_BUF[y][x];
 
-        for (I16 y = 0; y < h; ++y) {
-            for (I16 x = 0; x < w; ++x) {
-                U8 live_neighbors = 0;
-                for (I16 ny = y-1; ny <= y+1; ++ny)
-                    for (I16 nx = x-1; nx <= x+1; ++nx)
-                        live_neighbors += OLD_BUF[(ny+h) % h][(nx+w) % w];
-                live_neighbors -= OLD_BUF[y][x];
+            NEW_BUF[y][x] =
+                live_neighbors == 3 ||
+                live_neighbors == 2 && OLD_BUF[y][x];
 
-                NEW_BUF[y][x] =
-                    live_neighbors == 3 ||
-                    live_neighbors == 2 && OLD_BUF[y][x];
-
-                char* cell_text = OLD_BUF[y][x] ? ALIVE_TEXT : DEAD_TEXT;
-                while (*cell_text != '\0')
-                    *text_ptr++ = *cell_text++;
-            }
-            *text_ptr++ = '\n';
+            char* cell_text = OLD_BUF[y][x] ? ALIVE_TEXT : DEAD_TEXT;
+            while (*cell_text != '\0')
+                *text_ptr++ = *cell_text++;
         }
-
-        clear_terminal();
-        write_stdout(text_buf, text_ptr - text_buf);
-
-        buf_idx_toggle ^= 1;
-        sleep_fractional(0.1);
+        *text_ptr++ = '\n';
     }
+
+    clear_terminal();
+    write_stdout(text_buf, text_ptr - text_buf);
+
+    buf_idx_toggle ^= 1;
 }
 
-static void write_stdout(char* buf, U16 buf_size) {
-    #ifdef LINUX_64_BIT
-        int dummy_ret;
-        asm volatile(
-            "syscall"
-            : "=a"(dummy_ret)
-            : "a"(1), "D"(1), "S"(buf), "d"(buf_size)
-            : "memory");
-    #endif
-    #ifdef LINUX_32_BIT
-        int dummy_ret;
-        asm volatile(
-            "int $0x80"
-            : "=a"(dummy_ret)
-            : "a"(0x04), "b"(1), "c"(buf), "d"(buf_size)
-            : "memory");
-    #endif
-}
+#ifndef WEB
+    void write_stdout(char* buf, U16 buf_size) {
+        #ifdef LINUX_64_BIT
+            int dummy_ret;
+            asm volatile(
+                "syscall"
+                : "=a"(dummy_ret)
+                : "a"(1), "D"(1), "S"(buf), "d"(buf_size)
+                : "memory");
+        #endif
+        #ifdef LINUX_32_BIT
+            int dummy_ret;
+            asm volatile(
+                "int $0x80"
+                : "=a"(dummy_ret)
+                : "a"(0x04), "b"(1), "c"(buf), "d"(buf_size)
+                : "memory");
+        #endif
+    }
+    void clear_terminal(void) {
+        write_stdout("\033c", 2);
+    }
 
-static void sleep_fractional(float secs) {
-    unsigned long whole_secs = secs/1;
-    unsigned long nonosecs   = (secs - (float) whole_secs)*1e9;
+    void update_every_n_secs(float secs) {
+        unsigned long whole_secs = secs/1;
+        unsigned long nonosecs   = (secs - (float) whole_secs)*1e9;
+        unsigned long fake_timespec[2] = {whole_secs, nonosecs};
 
-    unsigned long fake_timespec[2] = {whole_secs, nonosecs};
-    #ifdef LINUX_64_BIT
-        int dummy_ret;
-        asm volatile(
-            "syscall"
-            : "=a"(dummy_ret)
-            : "a"(35), "D"(fake_timespec), "S"(0)
-            : "memory");
-    #endif
-    #ifdef LINUX_32_BIT
-        int dummy_ret;
-        asm volatile(
-            "int $0x80"
-            : "=a"(dummy_ret)
-            : "a"(0xa2), "b"(fake_timespec), "c"(0)
-            : "memory");
-    #endif
-}
-
-static void clear_terminal(void) {
-    write_stdout("\033c", 2);
-}
+        while (1) {
+            update();
+            #ifdef LINUX_64_BIT
+                int dummy_ret;
+                asm volatile(
+                    "syscall"
+                    : "=a"(dummy_ret)
+                    : "a"(35), "D"(fake_timespec), "S"(0)
+                    : "memory");
+            #endif
+            #ifdef LINUX_32_BIT
+                int dummy_ret;
+                asm volatile(
+                    "int $0x80"
+                    : "=a"(dummy_ret)
+                    : "a"(0xa2), "b"(fake_timespec), "c"(0)
+                    : "memory");
+            #endif
+        }
+    }
+#endif
 
